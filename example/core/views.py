237932497806d5f48622c8ec50905500d399af30
weizhou <weizhou@redhat.com>
2013-05-30 14:46:18 +0800

    Called attachment in example.

--- example/core/views.py
+++ example/core/views.py
@@ -6,14 +6,6 @@ from django.views.decorators.csrf import csrf_protect
 
 def home(request):
     context = {
-        'messages': Message.objects.all(),
+        'reports': Message.objects.all(),
     }
     return render_to_response('core/home.html', context, context_instance=RequestContext(request))
-
-
-@csrf_protect
-def message(request, id):
-    context = {
-        'message': get_object_or_404(Message, pk=id),
-    }
-    return render_to_response('core/message.html', context, context_instance=RequestContext(request))
--- example/urls.py
+++ example/urls.py
@@ -9,6 +9,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'core.views.home'),
-    url(r'^message/(?P<id>.+)$', 'core.views.message', name='message_detail'),
     url(r'^attachments/', include('attachments.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
